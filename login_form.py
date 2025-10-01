62ae9a0fa279a646ed09ca0fbda61d762a70788c
Arham Afzal <wwwarhamafzal682@gmail.com>
2025-10-01 12:36:52 +0500

    Day 6: Worked on POST vs GET methods and form handling in Flask

---
+++ login_form.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('form.html')
+
+
+@app.route('/login', methods=['POST'])
+def submit():
+    username = request.form.get('name')
+    email = request.form.get('email')
+    message = request.form.get('message')
+
+    if not username or not email or not message:
+        return "❌ All fields are required!"
+
+    return render_template('response.html', username=username, email=email, message=message)
+
+
+if __name__ == '__main__':
+    app.run(debug=True,port=8000)
