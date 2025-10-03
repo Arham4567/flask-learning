0cdbec6157067c4fb809f92ac9fedb515f760db0
Arham Afzal <wwwarhamafzal682@gmail.com>
2025-10-03 11:32:30 +0500

    Added portfolio and contact page with Flask routing

--- login_form.py
+++ login_form.py
@@ -19,6 +19,9 @@ def submit():
 
     return render_template('response.html', username=username, email=email, message=message)
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True,port=8000)
